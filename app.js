a016e336bd020c2237cb58fe75dd5662b66fb627
KevinLZC <klazaro0@ucol.mx>
2023-02-08 17:41:47 -0600

    Práctica 1: Celsius

---
+++ app.js
@@ -0,0 +1,8 @@
+const celsius = require("celsius");
+
+console.log("Convertir a celsius:", celsius.toCelsius(120, 2));
+console.log("Convertir a Celsius con caracter:", celsius.toCelsiusFmt(120, 2));
+console.log("Convertir a fahrenheit:", celsius.toFahrenheit(35, 2));
+console.log("Convertir a fahrenheit con caracter:", celsius.toFahrenheitFmt(35, 2));
+
+
